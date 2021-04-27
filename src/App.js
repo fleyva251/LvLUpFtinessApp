f446bb9d9c7008d3345d5568cdab118c9e2dd2e2
shauncarr22 <shauncarr2010@hotmail.com>
2021-04-27 09:19:16 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <h1>Lvl Up Fitness</h1>
+    )
+  }
+}
+
+export default App
